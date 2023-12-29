dcfb2271a9fc2c74fadf5d5d61f8f80389b1181e
ivanosquis10 <ivanosquis10.12@gmail.com>
2023-12-28 23:04:35 -0400

    [updated]: updated the path of the 'toggle-theme' component in the header-dashboard component

--- components/dashboard/header-dashboard.tsx
+++ components/dashboard/header-dashboard.tsx
@@ -1,24 +1,28 @@
 import Link from 'next/link'
-import { ToggleTheme } from '@/components/toggle-theme/toggle-theme'
+import { getUserSession } from '@/actions'
+import { ToggleTheme } from '@/components/shared/toggle-theme/toggle-theme'
 import { UserInfo } from '@/components/dashboard/user-info'
 
-interface Props {
-  name: string
-}
+export const HeaderDashboard = async () => {
+  const session = await getUserSession()
 
-export const HeaderDashboard = ({ name }: Props) => {
+  const user = {
+    email: session?.user.email,
+    photo: session?.user.user_metadata.avatar_url
+  }
+
   return (
     <header className="flex flex-col md:flex-row lg:h-[60px] items-center md:p-6 p-4 border-b w-full gap-2 lg:gap-5 dark:bg-zinc-900 bg-white">
       <nav className="relative flex items-center justify-between flex-1 w-full gap-4">
         <Link className="text-lg font-semibold" href="/dashboard">
-          Sub-status
+          Subslify
         </Link>
         <ul className="flex items-center gap-3 ml-auto">
           <li>
             <ToggleTheme />
           </li>
           <li>
-            <UserInfo />
+            <UserInfo user={user} />
           </li>
         </ul>
       </nav>
